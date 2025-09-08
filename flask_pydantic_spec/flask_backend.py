b1ea77fb2d1374049e77b0385bf7bbfe0967d549
Jack Keane <jack.keane@verkada.com>
2025-09-08 15:21:41 -0700

    linting

--- flask_pydantic_spec/flask_backend.py
+++ flask_pydantic_spec/flask_backend.py
@@ -195,7 +195,9 @@ class FlaskBackend:
 
         before(request, response, req_validation_error, None)
         if req_validation_error:
-            assert response is not None  # response is set when req_validation_error occurs
+            assert (
+                response is not None
+            )  # response is set when req_validation_error occurs
             abort(response)
 
         response = make_response(func(*args, **kwargs))
